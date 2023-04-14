3c0dfbaf616ed643eab1cfc0d68a3cf68cb6f996
Bluewhale97 <71245576+Bluewhale97@users.noreply.github.com>
2023-04-14 17:11:53 -0400

    Create operators_type.py

---
+++ operators_type.py
@@ -0,0 +1,7 @@
+# operators
+#arithmetic operators : +, -, *, /, %, **, //
+# assignment operators: =, +=, -=, *=, /=, %=, //=, **=, &=, |=, >>=, <<=, ^=
+# logic operators: and, or, not
+# identity operators: is, is not
+# membership operators: in, not in
+# bitwise operators: &, |, ^, ~, <<, >>

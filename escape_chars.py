ac6545d06f7c55805100217818d3d8b8592e7aa8
Bluewhale97 <71245576+Bluewhale97@users.noreply.github.com>
2023-04-14 17:04:43 -0400

    Create escape_chars.py

---
+++ escape_chars.py
@@ -0,0 +1,31 @@
+# escape character
+print("i am 23, i like travling and studying, and so on")
+
+# single quote \", to add a singlel quote in a string like the below:
+print("i am 23, i like \"travling\" and studying, and so on")
+
+# backslash \\, it equals to add a \ in that string
+print("i am 23, i like travling and\ studying, and so on")
+
+# new line, \n
+print("i am 23, i like travling and studying, \nand so on")
+
+# carriage return \r, move the words after the \r to the begining of the string
+print("i am 23, i like travling and studying, \rand so on")
+
+# tab, \t, new whitespace
+print("i am 23, i like travling and studying, \t\tand so on")
+
+# backspace, \b, move back a unit space
+print("i am 23, i like travling and studying, \band so on")
+
+# form feed, \f, seems in this env it doesnt work well? using \f has different effect on varying envs i guess?
+print("i am 23, i like travling and studying\f, and so on") 
+
+# octal value 
+oct(12421)
+int('0o30205',0)
+
+# hex value
+hex(12421)
+int('0x3085',0)

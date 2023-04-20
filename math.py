0efcef637e6ed8c128919b0fd89c47f40c637fa3
Bluewhale97 <71245576+Bluewhale97@users.noreply.github.com>
2023-04-20 15:54:01 -0400

    Create math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+#maths
+#min
+min([1,2,3])
+#max
+max([1,2,3],[4,5])
+#abs
+abs(-7.25)
+#power
+pow(3,2)
+
+#there is a built in module called math, you can import it to perform maths
+import math
+math.sqrt(64)
+math.ceil(4.12)
+math.floor(6.12)
+math.pi*1
+
